5d2b87d9a5b8a1926537d6974d05c8471d0bc8dd
Sergey <example@gmail.com>
2020-09-09 16:04:07 +0400

    add mailer, scroll and animation

--- src/js/script.js
+++ src/js/script.js
@@ -22,8 +22,8 @@ $(document).ready(function(){
     });
     
     function toggleSlide(item) {
-        $(item).each(function (i) {
-            $(this).on('click', function (e) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -36,24 +36,27 @@ $(document).ready(function(){
 
     //Modal
 
-    $('[data-modal=consultation').on('click', function () {
+    $('[data-modal=consultation').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
     });
-    $('.modal__close').on('click', function () {
+    $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
 
-    $('.button_mini').each(function (i) {
-        $(this).on('click', function () {
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
         })
     });
 
-    function validateForms(form) {
+    function validateForms(form){
         $(form).validate({
             rules: {
-                name: "required",
+                name: {
+                    required: true,
+                    minlength: 2
+                },
                 phone: "required",
                 email: {
                     required: true,
@@ -61,7 +64,10 @@ $(document).ready(function(){
                 }
             },
             messages: {
-                name: "Пожалуйста, введите свое имя",
+                name {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
                   required: "Пожалуйста, введите свой почту",
@@ -76,4 +82,36 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
