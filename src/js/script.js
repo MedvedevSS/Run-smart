6e727413d2245929e005b5bdd6261de5e560d360
Sergey <example@gmail.com>
2020-09-10 12:48:31 +0400

    add animate.css and wow.js

--- src/js/script.js
+++ src/js/script.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
             }
         ]
     });
-
+    
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-    
+
     function toggleSlide(item) {
         $(item).each(function(i) {
             $(this).on('click', function(e) {
@@ -34,13 +34,13 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-    //Modal
+    // Modal
 
-    $('[data-modal=consultation').on('click', function() {
+    $('[data-modal=consultation]').on('click', function() {
         $('.overlay, #consultation').fadeIn('slow');
     });
     $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
 
     $('.button_mini').each(function(i) {
@@ -50,7 +50,7 @@ $(document).ready(function(){
         })
     });
 
-    function validateForms(form) {
+    function validateForms(form){
         $(form).validate({
             rules: {
                 name: {
@@ -65,12 +65,12 @@ $(document).ready(function(){
             },
             messages: {
                 name: {
-                required: "Пожалуйста, введите свое имя",
-                minlength: jQuery.validator.format("Введите {0} символа!")
-              },
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
                 phone: "Пожалуйста, введите свой номер телефона",
                 email: {
-                  required: "Пожалуйста, введите свой почту",
+                  required: "Пожалуйста, введите свою почту",
                   email: "Неправильно введен адрес почты"
                 }
             }
@@ -117,3 +117,4 @@ $(document).ready(function(){
 
     new WOW().init();
 });
+
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,10 +11,10 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'nenuzhno777@mail.ru';                 // Наш логин
+$mail->Password = 'nhbfuhenhbrf34rus';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
